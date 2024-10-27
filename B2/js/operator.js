1b5fbc0a9e6bd8294c45631bfcdc498a1d498946
TuananHecker <93504319+TuananHecker@users.noreply.github.com>
2024-10-27 11:27:03 +0700

    first commit

---
+++ B2/js/operator.js
@@ -0,0 +1,19 @@
+// // 1. Toán tử số học (Arithmetic) (+-*/%) -----------------------------------
+// var a = 12 **2  //Lũy thừa
+// // console.log(a)
+// // // *** 1. Tiền tố (prefix) (++a, --a)
+// // console.log(++a) // 145
+// // console.log(a) //145
+
+// // *** 1. Hậu tố (postfix) (a++, a--)
+// console.log(a++) //144
+// console.log(a) //145
+
+// console.log(12 % 2) // chia lấy phần dư
+// console.log(13 / 2)
+
+// 2. Toán tử gán (Assignment) (= -= += *= /= **=)-------------------------------------------------
+var x = 10;
+x += 100 // x = x + 100
+
+// 3. Toán tử so sánh (Comparison) (> < >= <= != == ===)-----------------------------------
